eef85857467329d9d3e1fc67676b69eee2d9d063
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-09-29 14:16:28 +0900

    검색어 필터

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,15 +1,33 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 function TodoList({todo}) {
+
+    const [search, setSearch] = useState("");
+
+    function onChangeSearch(e) {
+        setSearch(e.target.value);
+    }
+
+    // 검색어 필터링 함수
+    function getSearchResult() {
+        if(search === "") {
+            return todo;
+        } else {
+            return todo.filter((it) => it.content.toLowerCase().includes(search.toLowerCase()));
+        }
+    }
+
+
 
     return (
         <div className="TodoList">
             <h4>Todo List 🌱</h4>
-            <input className="searchbar" placeholder="검색어를 입력하세요" />
+            <input className="searchbar" value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
             <div className="list_wrapper">
                 {/* for문 형태로 찍어줘야 함 */}
-                {todo.map((it) => (
+                {getSearchResult().map((it) => (
                     <div>
                         <TodoItem key={it.id} {...it} />
                         {/* {id, content, isDone, createdDate} -> TodoItem 컴포넌트에 전달 */}
